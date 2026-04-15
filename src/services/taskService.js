c7a4fc00bac0796b757e34b8f25ec6646ba63336
Peter Yale <Peterayale@gmail.com>
2026-04-14 23:03:11 -0400

    Fixed syntax error

--- src/services/taskService.js
+++ src/services/taskService.js
@@ -1,7 +1,7 @@
 import * as taskRepository from '../repositories/taskRepo.js';
 
 export async function getAllTasks(completed) {
-  return await taskRepo.findAll(completed);
+  return await taskRepository.findAll(completed);
 }
 
 export async function createTask(newTask) {
